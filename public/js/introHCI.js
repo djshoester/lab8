eedc43430fcd1ab578c40200a874bb0476bd2e5f
DjShoester <bhsu1228@gmail.com>
2014-02-27 06:21:53 +0000

    added experiment header

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -3,11 +3,14 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	$(".btn.btn-default.likeBtn").click(like_click);
 });
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(function(){
+		ga("send","event", "Category", "Action");
+	});
 }
